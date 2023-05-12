66d24089e2ab63729be254e5c54c7ce994daa57a
ManGroovia <127965606+ManGroovia@users.noreply.github.com>
2023-05-12 20:22:25 +0600

    added atoms components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,23 @@
 import Modal from "./Modal";
-import React from 'react'
+import React from "react";
+import { TopBtns } from "./atoms/Buttons";
 const Header = () => {
   const [modalIsOpen, setModalIsOpen] = React.useState<boolean>(false);
   const onCloseModal = () => setModalIsOpen(false);
   return (
     <div className="header">
-      <Modal active={modalIsOpen} onClose={onCloseModal}></Modal>
+      {modalIsOpen && (
+        <Modal active={modalIsOpen} onClose={onCloseModal}></Modal>
+      )}
       <div className="right">
         <div className="logo">
           <img src="icons/logo.svg" alt="logo" />
         </div>
         <div className="agent">
-          <p>От Японии до Владивостока <br /> один шаг с Токидоки</p>
-          <li>Агентский договор</li>
+          <p>
+            От Японии до Владивостока <br /> один шаг с Токидоки
+          </p>
+          <TopBtns>Агентский договор</TopBtns>
         </div>
         <div className="icons">
           <img src="icons/vk.svg" alt="" />
@@ -23,11 +28,14 @@ const Header = () => {
       <div className="left">
         <div className="contact">
           <a href="#">8(383)2-555-007</a>
-          <li onClick={()=> setModalIsOpen(true)}>Заказать звонок</li>
+
+          <TopBtns onClick={() => setModalIsOpen(true)}>
+            Заказать звонок
+          </TopBtns>
         </div>
         <button className="topBtn">
-        <img src="icons/dogovor.svg" alt="" />
-        Заключить договор
+          <img src="icons/dogovor.svg" alt="" />
+          Заключить договор
         </button>
       </div>
     </div>
---
+++ components/atoms/Buttons.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const TopBtns = styled.li`
+  color: #dd392b;
+  border-bottom: 1px solid #dd392b;
+  cursor: pointer;
+  border-bottom: 1px solid #dd392b;
+  width: 155px;
+  font-weight: 500;
+  font-size: 15px;
+  white-space: nowrap;
+  margin-top: 10px;
+`;
