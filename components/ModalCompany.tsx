f5e25e40fed6dbffb9be666e466f5268d5908f24
ManGroovia <marhyz@mail.ru>
2023-05-21 11:30:27 +0300

    updated styles in modals

--- components/ModalCompany.tsx
+++ components/ModalCompany.tsx
@@ -1,5 +1,4 @@
 import React, { PropsWithChildren, useRef } from "react";
-
 interface IModalProps {
   active: boolean;
   onClose: () => void;
@@ -14,10 +13,18 @@ const ModalCompany = ({ active, onClose }: PropsWithChildren<IModalProps>) => {
       className={active ? "modalCompany active" : "modalCompany"}
       onClick={onClose}
     >
-      <div className="oNas" onClick={(Event) => Event.stopPropagation()}>
+      <div className="oNas" onClick={(event) => event.stopPropagation()}>
         <div className="ssylky">
           <li>О компании</li>
-          <img src="icons/Vector.svg" alt="" />
+          <svg
+            width="7"
+            height="10"
+            viewBox="0 0 7 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1 9L5 5L1 1" stroke="#D2D3D8" strokeWidth="1.5" />
+          </svg>
         </div>
         <div className="ssylky">
           <li>Наши сотрудники</li>
@@ -49,3 +56,54 @@ const ModalCompany = ({ active, onClose }: PropsWithChildren<IModalProps>) => {
 };
 
 export default ModalCompany;
+// interface IModalProps {
+//   active: boolean;
+//   onClose: () => void;
+// }
+
+// const ModalCompany = ({ active, onClose }: PropsWithChildren<IModalProps>) => {
+//   if (!active) {
+//     return null;
+//   }
+//   return (
+//     <div
+//       className={active ? "modalCompany active" : "modalCompany"}
+//       onClick={onClose}
+//     >
+//       <div className="oNas" onClick={(Event) => Event.stopPropagation()}>
+//         <div className="ssylky">
+//           <li>О компании</li>
+//           <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M1 9L5 5L1 1" stroke="#D2D3D8" stroke-width="1.5"/>
+// </svg>
+//         </div>
+//         <div className="ssylky">
+//           <li>Наши сотрудники</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//         <div className="ssylky">
+//           <li>Автопарк Токидоки</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//         <div className="ssylky">
+//           <li>Офис Токидоки</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//         <div className="ssylky">
+//           <li>Агентский договор</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//         <div className="ssylky">
+//           <li>Учредительные документы</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//         <div className="ssylky">
+//           <li>Видео</li>
+//           <img src="icons/Vector.svg" alt="" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ModalCompany;
